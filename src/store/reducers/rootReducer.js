13ee88424ff519f5928907d208be4916a096a01f
Aleksandr Hotskiy <63182612+hotsk1y@users.noreply.github.com>
2021-08-04 22:34:21 +0300

    Updates

--- src/store/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { mainPageReducer } from './mainPageReducer'
-import { weatherCardReducer } from './weatherCardReducer'
-
-export const rootReducer = combineReducers({
-  mainPage: mainPageReducer,
-  weatherCard: weatherCardReducer,
-})
